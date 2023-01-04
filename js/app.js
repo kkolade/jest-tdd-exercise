19624f7d5c9459f2cdea1da4422119bf10a6c7eb
Kola Kolade <kolakolade@gmail.com>
2023-01-04 19:43:55 +0100

    Function expanded for further edge case

--- js/app.js
+++ js/app.js
@@ -1,4 +1,13 @@
 function stringLength(string) {
+  if (typeof string !== "string") {
+    throw new Error("Input must be a string");
+  }
+  if (string.length === 0) {
+    throw new Error("Input string must not be empty");
+  }
+  if (string.length > 10) {
+    throw new Error("Input string must not be greater than 10 characters");
+  }
   return string.length;
 }
 
--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,19 @@
 const stringLength = require("../js/app");
-const string = "I am Kola";
 
-test("Return length of supplied string", () => {
-  expect(stringLength(string)).toEqual(9);
+test("Error if input is not a string", () => {
+  expect(() => stringLength(123)).toThrow("Input must be a string");
+});
+
+test("Error if input string is empty", () => {
+  expect(() => stringLength("")).toThrow("Input string must not be empty");
+});
+
+test("Error if input string is greater than 10 characters", () => {
+  expect(() => stringLength("I am Kola the greatest")).toThrow(
+    "Input string must not be greater than 10 characters"
+  );
+});
+
+test("Correct string length", () => {
+  expect(stringLength("I am Kola")).toBe(9);
 });
